e9d8c9b5700ca439305709eb34164b330d19d7ab
Uriegas <1930526@upv.edu.mx>
2021-02-18 12:41:09 -0600

    Piece Class

--- Piece.java
+++ Piece.java
@@ -1,4 +1,28 @@
 
+enum Color{WHITE, BLACK};
+enum Alive{LIVE, DEAD};
+
 public abstract class Piece {
-    private Coordinate position = new Coordinate();
+    private Coordinate position;
+    private Color color;
+    private Alive alive;
+
+    public Piece(Color color){
+        this.color = color;
+        this.alive = Alive.LIVE;
+    }
+
+    public void setPosition(Coordinate other){
+        position.setPosition(other);
+    }
+
+    public Coordinate getPosition(){
+        return position;
+    }
+
+    public String toString(){
+        return  (this.color == Color.WHITE ? "WHITE" : "BLACK") +
+                (this.alive == Alive.LIVE ? "LIVE" : "DEAD");
+    }
+
 }
