e8b9081afbee2df2ba814b136af4c3cdf1b18bb2
Uriegas <1930526@upv.edu.mx>
2021-02-18 13:27:23 -0600

    Possible Moves of King

--- King.java
+++ King.java
@@ -1,9 +1,22 @@
 import java.util.*;
 
 public class King extends Piece{
-    private List<Coordinate> movements = new ArrayList<Coordinate>();
+//    private Coordinate[] possibleMoves = new Coordinate[4];
+
+    private List<Coordinate> mvm = new ArrayList<Coordinate>();
+    private void initPossibleMoves(){
+        this.mvm.add(new Coordinate(0, 1));
+        this.mvm.add(new Coordinate(0, -1));
+        this.mvm.add(new Coordinate(1, 0));
+        this.mvm.add(new Coordinate(-1, 0));
+        this.mvm.add(new Coordinate(1, 1));
+        this.mvm.add(new Coordinate(1, -1));
+        this.mvm.add(new Coordinate(-1, 1));
+        this.mvm.add(new Coordinate(-1, -1));
+    }
 
     public King(Color color){
         super(color);
+        this.initPossibleMoves();
     }
 }
