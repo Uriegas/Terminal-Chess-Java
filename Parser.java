7ddd535fe6c55c633d56fc7a8cf8a4398c5eff33
Uriegas <1930526@upv.edu.mx>
2021-02-18 13:54:38 -0600

    Parser

---
+++ Parser.java
@@ -0,0 +1,9 @@
+//Parse from String to Coordinate
+//Ex. 1f -> (0, 4)
+public class Parser extends Coordinate{
+    public Coordinate Parse(String movement){
+        Coordinate position = new Coordinate();
+
+        return position;
+    }
+}
