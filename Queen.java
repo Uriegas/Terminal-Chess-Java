1768fc0b635f33a5d7defedeaf5bd5455dc44f55
Uriegas <1930526@upv.edu.mx>
2021-02-18 15:46:17 -0600

    Queen Class

---
+++ Queen.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Queen extends Piece{
+    private char figure = '♛';
+
+    private List<Coordinate> mvm = new ArrayList<Coordinate>();
+    private void initPossibleMoves(){
+        //Esto posiblemente este mal, porque la reina no se mueve asi
+        this.mvm.add(new Coordinate(0, 1));
+        this.mvm.add(new Coordinate(0, -1));
+        this.mvm.add(new Coordinate(1, 0));
+        this.mvm.add(new Coordinate(-1, 0));
+        this.mvm.add(new Coordinate(1, 1));
+        this.mvm.add(new Coordinate(1, -1));
+        this.mvm.add(new Coordinate(-1, 1));
+        this.mvm.add(new Coordinate(-1, -1));
+    }
+
+    public Queen(Color color, Coordinate position){
+        super(color, position);
+        this.initPossibleMoves();
+    }
+    public char toChar(){
+        return figure;
+    }
+}
