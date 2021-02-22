e86673406b3eb11a9babb4f5ab20ad0729ea42b8
Uriegas <1930526@upv.edu.mx>
2021-02-21 20:06:25 -0600

    TestKing con Parser

--- TestKing.java
+++ TestKing.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class TestKing {
     public static void main(String[] args) {
-        King myking = new King();
+        Scanner in = new Scanner(System.in);//Para leer teclado
+        King myking = new King();//Instanciar King
+        Parser userInput = new Parser();//Parser
+        String s;//Guarda lo que ingreso el usuario
+
+        //Imprimir toda la info del rey
         System.out.println(myking.toString());
+
+        //Integrando Parser para mover coordenadas
+        while(true){
+            System.out.println("> ");
+            s = in.nextLine();
+
+            //Pasa de string a coordenada y cambia la coordenada del rey
+            myking.setPosition( userInput.Parse(s) );
+            System.out.println(myking.getPosition().toString());
+        }
     }
 }
