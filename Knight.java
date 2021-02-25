157da709f9438fb44b7657fed0a8179a1527443a
roberto <1930346@upv.edu.mx>
2021-02-24 18:26:39 -0600

    Piezas restantes y colorsitos uwu

---
+++ Knight.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Knight extends Piece{
+    private char figure = '♞';
+
+    private List<Coordinate> mvm = new ArrayList<Coordinate>();
+    
+    private void initPossibleMoves(){
+        //Esto posiblemente este mal, porque el alfil no se mueve asi, sino completos
+        this.mvm.add(new Coordinate(2, 1));
+        this.mvm.add(new Coordinate(2, -1));
+        this.mvm.add(new Coordinate(-2, 1));
+        this.mvm.add(new Coordinate(-2, -1));
+        this.mvm.add(new Coordinate(1, 2));
+        this.mvm.add(new Coordinate(-1, 2));
+        this.mvm.add(new Coordinate(1, -2));
+        this.mvm.add(new Coordinate(-1, -2));
+        
+    }
+
+    public Knight(Color color, Coordinate position){
+        super(color, position);
+        this.initPossibleMoves();
+    }
+    public char toChar(){
+        return this.figure;
+    }
+    
+    //Prueba Viernes  ¿? XD, le puse donde debería estar 1 vdd
+    public Knight(){
+        super(Color.WHITE, new Coordinate(1,0));
+    }
+    //
+
+    //Esto se haría implementando lo de isaac
+    @Override
+    public String getName(){
+        return "Knight";
+    }
+
+}
--- Main.java
+++ Main.java
@@ -8,12 +8,12 @@ public class Main {
 		Queen queen = new Queen();
 
 		//Agrega los Reyes
-		board.setPiece(String.valueOf(king.getFigure()), 0, 4);
-		board.setPiece(String.valueOf(king.getFigure()), 7, 4);
+		board.setPiece("\033[;31m" + String.valueOf(king.getFigure()) + "\033[;37m", 0, 4);
+		board.setPiece("\033[;34m" + String.valueOf(king.getFigure()) + "\033[;37m", 7, 4);
 
 		//Agrega las Reinas
-		board.setPiece(String.valueOf(queen.toChar()), 0, 3);
-		board.setPiece(String.valueOf(queen.toChar()), 7, 3);
+		board.setPiece("\033[;31m" + String.valueOf(queen.toChar()) + "\033[;37m", 0, 3);
+		board.setPiece("\033[;34m" + String.valueOf(queen.toChar()) + "\033[;37m", 7, 3);
 
 		//Agrega peones en la 2da y 7ma fila
 		for(int i = 0; i < board.getFilas(); i++)
@@ -21,14 +21,13 @@ public class Main {
 			for(int j = 0; j < board.getColumnas(); j++)
 			{
 				//System.out.println("Entra");
-				if(i == 1 || i == 6)
-				{
-					board.setPiece(String.valueOf(pawn.toChar()), i, j);
-					board.setPiece(String.valueOf(pawn.toChar()), i, j);
-				}
-				else if(board.getPosition(i, j) == null)
-				{
-					board.setPiece(String.valueOf("x"), i, j);
+				if(i == 1){
+					board.setPiece("\033[;31m" + String.valueOf(pawn.toChar()) + "\033[;37m" , i, j);
+					//board.setPiece(String.valueOf(pawn.toChar()), i, j);
+				}else if(i == 6){
+					board.setPiece("\033[;34m" + String.valueOf(pawn.toChar()) + "\033[;37m", i, j);
+				}else if(board.getPosition(i, j) == null){
+					board.setPiece("\033[;37m" + String.valueOf("x") + "\033[;37m", i, j);
 				}
 			}
 		}
@@ -37,7 +36,7 @@ public class Main {
 		String filasCoordenadas =" ";
 		for(int i = 0; i < board.getFilas(); i++)
 		{
-			linea = String.valueOf(8-i + " ");
+			linea = "\033[;37m" + String.valueOf(8-i + " ") + "\033[;37m";
 			for(int j = 0; j < board.getColumnas(); j++)
 			{
 				if(j == 7)
