524f7e620eb9915ff23047c3bd80afa2170837b6
Uriegas <1930526@upv.edu.mx>
2021-02-25 12:42:54 -0600

    drawBoard formato

--- Board.java
+++ Board.java
@@ -60,23 +60,32 @@ public class Board
 		Coordinate currentPosition;
 		//La neta no tengo idea de como pasar esto a un string
 		for(int y=0; y<8; y++){
+			//Imprimir los numeros de filas
+			this.boardDrawed += String.valueOf(8-y) + "  ";
 			for(int x=0; x<8; x++){//Recorrer filas
 				//Antes de iterar sobre cada pieza, guarda el tamaño del boardDrawed y crea una coordenada
 				sizeOfBoard = this.boardDrawed.length();
 				currentPosition = new Coordinate(x, y);
 				for( int i=0; i<pieces.size(); i++){//Iterar sobre todas las piezas
 					if( currentPosition.equals( pieces.get(i).getPosition() ) ){//Si la pieza esta en la coordenada iterable, entonces agregala al string
-						this.boardDrawed += pieces.get(i).getFigure();
+						//Seleccionar el color de la pieza segun la variable Color
+						if(pieces.get(i).getColor() == Color.WHITE)//Azul
+							this.boardDrawed += "\033[;34m" + pieces.get(i).getFigure() + "\u001B[0m" + ' ';
+						else//Rojo
+							this.boardDrawed += "\033[;31m" + pieces.get(i).getFigure() + "\u001B[0m" + ' ';
+						//this.boardDrawed += pieces.get(i).getFigure();
 					}
 				}
 				//Verificar si en realidad se agrego algo a la casilla x,y
 				if(sizeOfBoard == this.boardDrawed.length()){//No se agregó nada, entonces dibuja un puntillo
-					this.boardDrawed += '·';
+					this.boardDrawed += "· ";
 				}
 			}
 			//Se acabo de recorrer una fila, entonces agrega un salto de linea
 			this.boardDrawed += '\n';
 		}
+		//Agregar la línea de "a b c d e f g h"
+		this.boardDrawed += "   a b c d e f g h";
 		return this.boardDrawed;
 	}
 
