9453778cc7add0a5d8a11eab481ff01962a084ed
Uriegas <1930526@upv.edu.mx>
2021-02-26 15:52:05 -0600

    Move Class

--- Move.java
+++ Move.java
@@ -1,7 +1,69 @@
-//Ya entendi esta clase chicas
-//Es el movimiento del jugador
-//Hay 2 coordenadas: origen y destino
-//Checa la validez del movimiento (creo)
+//Una clase que guarde toda la información de un movimiento
 public class Move {
+	//------------------
+	//Flags
+	//------------------
+    private boolean checkmate;
+    private boolean kingsideCastle;
+    private boolean queensideCastle;
+    private boolean promotion;
+    private boolean passant;
+    private boolean stalemate;
+    private boolean specialMovePiece;
+    private boolean rookMove;
 
+	//------------------
+	//Piece
+	//------------------
+    private Piece piece;
+    private Coordinate oldPos;
+    private Coordinate newPos;
+    private Piece pieceToCapture;
+
+
+    Move(Piece piece, Coordinate destionation, Piece toCapture){
+        this.checkmate = false;
+        this.kingsideCastle = false;
+        this.queensideCastle = false;
+        this.promotion = false;
+        this.passant = false;
+        this.stalemate = false;
+        this.specialMovePiece = false;
+        this.rookMove = false;
+        
+        this.piece = piece;
+        this.oldPos = this.piece.getPosition();
+        this.newPos = destionation;
+        this.pieceToCapture = toCapture;
+    }
+    
+    Move(Piece piece, Coordinate destionation){
+        this.checkmate = false;
+        this.kingsideCastle = false;
+        this.queensideCastle = false;
+        this.promotion = false;
+        this.passant = false;
+        this.stalemate = false;
+        this.specialMovePiece = false;
+        this.rookMove = false;
+        
+        this.piece = piece;
+        this.oldPos = this.piece.getPosition();
+        this.newPos = destionation;
+        this.pieceToCapture = null;
+    }
+
+    public String toString(){
+        return   "----Current  Piece----\n" + this.piece.toString()
+               + "---Piece to Capture---\n" + this.pieceToCapture.toString()
+               + "\n-------Flags--------"
+               + "\nCheckmate = " + Boolean.toString(this.checkmate)
+               + "\nKing Side Castle = " + Boolean.toString(this.kingsideCastle)
+               + "\nQueen Side Castle = " + Boolean.toString(this.queensideCastle)
+               + "\nPromotion = " + Boolean.toString(this.promotion)
+               + "\nPassant = " + Boolean.toString(this.passant)
+               + "\nStalemate = " + Boolean.toString(this.stalemate)
+               + "\nSpecial Move Piece = " + Boolean.toString(this.specialMovePiece)
+               + "\nRook Move = " + Boolean.toString(this.rookMove);
+    }
 }
